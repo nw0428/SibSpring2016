d8b282cf4aec8fba2c7e76aed935acb00c2e5420
Broderick Attra <barbaraharvey@Barbaras-MacBook-Air.local>
2016-03-07 10:42:29 -0500

    updated images, built about and experience pop-ups

--- js/attra/script.js
+++ js/attra/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
 	$('.navCircle').hover(
 	  function() {
 	    $(this).css({'border-width':'5px'});
@@ -13,8 +12,39 @@ $(document).ready(function(){
 	$('#homeCircle').hover(
 	  function() {
 	    $(this).css({'border-width':'5px'});
+	    $('.circleLabel').css({'visibility':'visible'});
 	  }, function() {
 	    $(this).css({'border-width':'10px'});
+	    $('.circleLabel').css({'visibility':'hidden'});
 	  }
 	);
+
+	$('.navCircle').click(function(){
+		var id = $(this).attr('id');
+		openSubviewFor(id);
+	});
+
+	$('.subViewExitBtn').hover(
+		function() {
+			$(this).css({'height':'55px', 'width':'55px'});
+		}, function() {
+			$(this).css({'height':'50px', 'width':'50px'});
+		}
+	).click(function() {
+		$(this).parent().fadeOut(100);
+	});
+
 });
+
+function openSubviewFor(id) {
+	$('.subView').fadeOut(100);
+	$('.circleLabel').css({'visibility':'hidden'});
+	switch(id) {
+		case 'infoCircle':
+			$('#aboutView').fadeIn(100);
+			break;
+		case 'experienceCircle':
+			$('#experienceView').fadeIn(100);
+			break;
+	}
+}
